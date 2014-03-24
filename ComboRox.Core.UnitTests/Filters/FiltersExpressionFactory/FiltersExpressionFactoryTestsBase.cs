9d50c09a18f8f2042d735d9d0c73bf585974108a
Simeon Nenov <nenov.simeon@gmail.com>
2014-03-24 21:43:30 +0200

    Refactored Unit tests

---
+++ ComboRox.Core.UnitTests/Filters/FiltersExpressionFactory/FiltersExpressionFactoryTestsBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComboRox.Core.UnitTests.Filters.FiltersExpressionFactory
+{
+    public class FiltersExpressionFactoryTestsBase
+    {
+        private List<UserTestingClass> collection;
+
+        public List<UserTestingClass> Collection
+        {
+            get { return this.collection; }
+        }
+
+        [TestInitialize]
+        public void InitializeData()
+        {
+            this.collection = new List<UserTestingClass>()
+                {
+                    new UserTestingClass
+                        {
+                            Id = new Guid(),
+                            FirstName = "John",
+                            LastName = "Doe",
+                            Age = 42
+                        },
+                    new UserTestingClass
+                        {
+                            Id = new Guid(),
+                            FirstName = "Peter",
+                            LastName = "Brown",
+                            Age = 27
+                        }
+                };
+        }
+
+        [TestCleanup]
+        public void ClearData()
+        {
+            this.collection.Clear();
+        }
+    }
+}
---
+++ ComboRox.Core.UnitTests/Filters/FiltersExpressionFactory/FiltersExpressionFactory_CreateFilterExpression_Should.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ComboRox.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace ComboRox.Core.UnitTests.Filters.FiltersExpressionFactory
+{
+    [TestClass]
+    public class FiltersExpressionFactory_CreateFilterExpression_Should : FiltersExpressionFactoryTestsBase
+    {
+        [TestMethod]
+        public void ReturnZeroItems_AfterFiltering()
+        {
+            // Arrange
+            Expression<Func<UserTestingClass, bool>> filterExpression = Core.Filters.FiltersExpressionFactory.Create<UserTestingClass>(new List<Filter>
+                {
+                    new Filter { Operator = Operator.Equals, PropertyName = "FirstName", Value = "John" },
+                    new Filter { Operator = Operator.Equals, PropertyName = "LastName", Value = "Brown" }
+                });
+
+            // Act
+            var result = Collection.AsQueryable().Where(filterExpression);
+
+            // Assert
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void ReturnCorrectExpression_WhenPassingOrExpresionFilters()
+        {
+            // Arrange
+            Expression<Func<UserTestingClass, bool>> filterExpression = Core.Filters.FiltersExpressionFactory.Create<UserTestingClass>(new List<Filter>
+                {
+                    new Filter
+                        {
+                            Operator = Operator.Equals, 
+                            PropertyName = "FirstName",
+                            Value = "John",
+                            OrFilters = new List<Filter> { new Filter { Operator = Operator.Equals, PropertyName = "LastName", Value = "Brown" } }
+                        }
+                });
+
+            // Act
+            var result = Collection.AsQueryable().Where(filterExpression);
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public void ReturnCorrectExpression_WhenPassingOrExpresionFiltersAndPropertiesToParse()
+        {
+            // Arrange
+            DateTime dateTimeNow = DateTime.Now;
+            UserTestingClass firstEntry = Collection.First();
+            firstEntry.TimeGenerated = dateTimeNow;
+
+            Expression<Func<UserTestingClass, bool>> filterExpression = Core.Filters.FiltersExpressionFactory.Create<UserTestingClass>(new List<Filter>
+                {
+                    new Filter
+                        {
+                            Operator = Operator.Equals, 
+                            PropertyName = "Id",
+                            Value = Guid.NewGuid().ToString(), // Fake ID
+                            OrFilters = new List<Filter> 
+                            { 
+                                new Filter
+                                {
+                                    Operator = Operator.Equals,
+                                    PropertyName = "TimeGenerated.Day", 
+                                    Value = dateTimeNow.Day.ToString()
+                                },
+                                new Filter
+                                {
+                                    Operator = Operator.Equals,
+                                    PropertyName = "TimeGenerated.Minute", 
+                                    Value = dateTimeNow.Minute.ToString()
+                                }
+                            }
+                    }
+                });
+
+            // Act
+            var result = this.Collection.AsQueryable().Where(filterExpression);
+
+            // Assert
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void ReturnCorrectExpression_WhenPassingEqualsNotEqualsExpresionFilters()
+        {
+            // Arrange
+            Expression<Func<UserTestingClass, bool>> filterExpression = Core.Filters.FiltersExpressionFactory.Create<UserTestingClass>(new List<Filter>
+                {
+                    new Filter
+                        {
+                        Operator = Operator.Equals, 
+                        PropertyName = "FirstName",
+                        Value = "Peter"
+                    },
+                    new Filter 
+                    { 
+                        Operator = Operator.NotEquals,
+                        PropertyName = "LastName",
+                        Value = "Doe" 
+                    }
+                });
+
+            // Act
+            var result = Collection.AsQueryable().Where(filterExpression);
+
+            // Assert
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
